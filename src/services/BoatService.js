08dca2a4b393ccb86c2c71cb673f5ea8ea676aca
Capaldi96 <capaldi_96@live.se>
2020-09-03 13:45:14 +0200

    latest code

---
+++ src/services/BoatService.js
@@ -0,0 +1,47 @@
+import axios from "axios";
+const url = 'http://localhost:5000';
+export default {
+  async getBoats() {  
+    let res = await axios.get(url + "/api/boats");
+    return res.data;
+  },
+  async getDetailedBoat(id) {
+    let res = await axios.get(url + "/api/boats/" + id);
+    return res.data;
+  },
+  async search(data){
+    let res = await axios.get(url + "/api/search?" + data);
+    return res.data;
+  },
+  addBoat(data){
+    axios.post(url + '/api/boats/add',{
+      modelName: data.modelName,
+      manufacturingYear: data.manufacturingYear,
+      price: data.price,
+      motor: data.motor,
+      sailboat: data.sailboat,
+      url: data.url
+    })
+    .then((response)=>{
+      console.log(response)
+    })
+  },
+  deleteBoat(id){
+    axios.delete(url + '/api/boats/delete/' + id)
+  },
+  editBoat(data){
+    axios.put(url + '/api/boats/edit/' + data._id, {
+      modelName: data.modelName,
+      manufacturingYear: data.manufacturingYear,
+      price: data.price,
+      motor: data.motor,
+      sailboat: data.sailboat,
+      url: data.url,
+    })
+  },
+  async resetDatabase(){
+    let res = await axios.get(url + "/api/resetmongodb");
+    return res.data;
+  }
+  
+};
---
+++ src/views/welcome.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="container">
+        <h1>Welcome captain</h1>
+        <h4>Check out our boat collection above!</h4>
+        <button class="myButton" @click="resetDatabase">Start/restart database</button>
+    </div>
+</template>
+
+<script>
+import BoatService from "@/services/BoatService.js";
+
+export default {
+    methods:{
+        resetDatabase(){
+            BoatService.resetDatabase();
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+    .container{
+        width:100%;
+        display: grid;
+        place-items: center;
+    }
+    .myButton{
+        margin-top:3em;
+    }
+    h1,h4{
+        color:#ffffff;
+        text-shadow: 2px 2px black;
+    }
+</style>
