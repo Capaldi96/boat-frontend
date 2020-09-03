96e733d06960f2226ac68044d53dc781c590dd7e
Capaldi96 <capaldi_96@live.se>
2020-09-03 15:04:10 +0200

    proxy settings

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,9 @@
 const path = require('path');
 
 module.exports = {
-    outputDir: path.resolve(__dirname, '../server/public'),
     devServer:{
         proxy:{
-            '/api':{
+            '^/api':{
                 target:'mongod://localhost:5000'
             }
         }
